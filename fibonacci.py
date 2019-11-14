1f55b1cdf094ebc5d6ca38b5b2c24b2e2d971171
Paris Porter Bradley <parisporterbradley@mines.edu>
2019-11-14 11:46:50 -0700

    created file that calculates the first 10 Fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+# fibonacci.py
+
+def fib():
+    fibs = [1, 2]
+
+    for i in range(1,9):
+
+        ''' 
+        implement Fibonacci sequence to calculate the 
+        first 10 Fibonacci numbers, note Fn = Fn-1 + Fn-2
+        '''
+
+        fibs.append(fibs[i]+fibs[i-1])
+
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
